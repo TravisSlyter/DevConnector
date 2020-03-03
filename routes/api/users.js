b7160a17eaaa685458ee28409445c8aeb771fecb
TravisSlyter <42150524+TravisSlyter@users.noreply.github.com>
2020-03-03 16:53:19 -0700

    added User models

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+// might need 'express-validator/check'
 
-// @route   Get api/users
-// @desc    Test route
+const User = require('../../models/User');
+
+// @route   Post api/users
+// @desc    Register users
 //@access   Public (no token)
-router.get('/', (req, res) => res.send('Users route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get users gravatar
+
+      // Encrypt password
+
+      // Return jswebtoken
+
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
