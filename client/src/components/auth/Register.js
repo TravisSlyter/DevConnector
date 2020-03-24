b19db04ac174e455913453d8f06f5a5c84a34c28
Travis Slyter <travisslyter@gmail.com>
2020-03-24 15:50:05 -0600

    auth reducer & Register action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,11 +2,12 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -23,7 +24,7 @@ const Register = ({ setAlert }) => {
         if (password !== password2) {
             setAlert('Passwords do not match', 'danger');
         } else {
-            console.log('Success')
+            register({ name, email, password })
         }
     };
 
@@ -39,7 +40,8 @@ const Register = ({ setAlert }) => {
           name="name" 
           value={name}
           onChange={e => onChange(e)}
-          required />
+          //required
+           />
         </div>
         <div className="form-group">
           <input 
@@ -48,7 +50,7 @@ const Register = ({ setAlert }) => {
           name="email"
           value={email}
           onChange={e => onChange(e)}
-          required
+          //required
            />
           <small className="form-text"
             >This site uses Gravatar so if you want a profile image, use a
@@ -85,7 +87,8 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 }
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
