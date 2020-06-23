c0a9f7e6b07f8c2d4aab0c4f68aa082f18ee06d2
Travis Slyter <travisslyter@gmail.com>
2020-06-23 16:18:33 -0600

    display Profiles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,11 @@ const Navbar = ({ auth: { isauthenticated, loading }, logout }) => {
     const authLinks = (
       <ul>
         <li>
+        <Link to="/profiles">
+        Developers
+        </Link>
+        </li>
+        <li>
         <Link to="/dashboard">
         <i className='fas fa-user'></i>{' '}
         <span className='hide-sm'>Dashboard</span>
@@ -24,6 +29,11 @@ const Navbar = ({ auth: { isauthenticated, loading }, logout }) => {
 
     const guestLinks = (
       <ul>
+        <li>
+        <Link to="/profiles">
+        Developers
+        </Link>
+        </li>
         <li>
           <a href="#!">Developers</a>
         </li>
