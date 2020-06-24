e1f3cdb3cb0f59305e6d7a9a38efc8dcdb083bc2
Travis Slyter <travisslyter@gmail.com>
2020-06-24 15:22:19 -0600

    fix console warnings

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -56,7 +56,7 @@ const EditProfile = ({
 
             instagram: loading || !profile.social ? '' : profile.social.instagram,
         });
-    }, [loading]);
+    }, [loading, getCurrentProfile]);
 
     const {
         company,
